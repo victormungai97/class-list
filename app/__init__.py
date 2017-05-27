762bbec5db17b14269f38b06422bbb26d7a14675
victormungai97 <victormungai97@gmail.com>
2017-05-27 22:52:51 +0300

    Update on views

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,8 @@ app = Flask(__name__, instance_relative_config=True)
 
 def create_app(config_name):
 	global app; global db
+	if config_name not in app_config.keys():
+		config_name = 'development'
 	app.config.from_object(app_config[config_name])
 	app.config.from_pyfile('config.py')
 	db.init_app(app)
