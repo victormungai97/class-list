b09faf119a8605d6febc5a9ffc8358e93adfbfeb
Victor Kamau <victormungai97@gmail.com>
2018-04-03 18:34:46 +0300

    Hello in April 2018

--- instance/config.py
+++
@@ -1,5 +0,0 @@
-# instance/config.py
-# sensitive information not to be pushed to version control
-
-SECRET_KEY = "my-secret-key"
-SQLALCHEMY_DATABASE_URI = '''mysql+pymysql://myuser:Hater4@life!@localhost/dreamteam'''
